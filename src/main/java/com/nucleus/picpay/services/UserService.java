142595b9ba4440fbc3883e7411142df9e809e1b2
FelipeRodrigues05 <fehzin_@outlook.com>
2024-06-05 20:18:15 -0300

    feat: removed user controller and createUser function

--- src/main/java/com/nucleus/picpay/services/UserService.java
+++ src/main/java/com/nucleus/picpay/services/UserService.java
@@ -24,10 +24,6 @@ public class UserService {
         return this.userRepository.findByDocument(document).orElseThrow(() -> new Exception("User not found"));
     }
 
-    public void createUser(UserDTO data) {
-        
-    }
-
     public void saveUser(User user) {
         this.userRepository.save(user);
     }
